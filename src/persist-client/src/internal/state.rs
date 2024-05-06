8f920e8d5cb5b19af150548f7f95145ee1a657f3
Ben Kirwin <bkirwi@materialize.com>
2024-05-06 11:53:38 -0400

    Revert "Merge pull request #26671 from bkirwi/spine-test"
    This reverts commit 1fff512be7c4f7aa784ddac014679999f02ec6ca, reversing
    changes made to 4b0f90f590dc0b0824e6a14935160a3f2253e441.

--- src/persist-client/src/internal/state.rs
+++ src/persist-client/src/internal/state.rs
@@ -1972,12 +1972,7 @@ pub(crate) mod tests {
         )
     }
 
-    pub(crate) fn hollow<T: Timestamp>(
-        lower: T,
-        upper: T,
-        keys: &[&str],
-        len: usize,
-    ) -> HollowBatch<T> {
+    fn hollow<T: Timestamp>(lower: T, upper: T, keys: &[&str], len: usize) -> HollowBatch<T> {
         HollowBatch {
             desc: Description::new(
                 Antichain::from_elem(lower),
